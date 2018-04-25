5b1855618ec8681a257d3e7e3c82e22ebec12723
David Zhang <crispgm@gmail.com>
2018-04-25 20:52:01 +0800

    Fix tests

--- test/test_jekyll_filter.rb
+++ test/test_jekyll_filter.rb
@@ -3,7 +3,7 @@ require "helper"
 class TestJekyllFilter < Minitest::Test
   context nil do
     should "build a jekyll site where spacifier filter works" do
-      `cd test/site && bundle exec jekyll build`
+      `cd test/site && bundle install && bundle exec jekyll build`
       output = File.read("test/site/_site/index.html")
       assert(true, output.include?("它在 works"))
       assert(true, output.include?("hello 世界"))
