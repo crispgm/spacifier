755b86f214fd6e51be1c20c5b18d493d2d76ab3a
David Zhang <crispgm@gmail.com>
2018-04-25 21:29:29 +0800

    Specify output path of jekyll

--- test/test_jekyll_filter.rb
+++ test/test_jekyll_filter.rb
@@ -3,7 +3,8 @@ require "helper"
 class TestJekyllFilter < Minitest::Test
   context nil do
     should "build a jekyll site where spacifier filter works" do
-      `cd ./test/site && bundle install && bundle exec jekyll build`
+      output_path = File.expand_path("./site/_site", File.dirname(__FILE__))
+      `cd ./test/site && bundle install && bundle exec jekyll build --destination=#{output_path}`
       output = File.read(File.expand_path("./site/_site/index.html", File.dirname(__FILE__)))
       assert(true, output.include?("它在 works"))
       assert(true, output.include?("hello 世界"))
