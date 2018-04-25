e8534d38253fa3bb9e28818abffda3711c52f4c6
David Zhang <crispgm@gmail.com>
2018-04-25 21:44:05 +0800

    Fix travis again

--- test/test_jekyll_filter.rb
+++ test/test_jekyll_filter.rb
@@ -3,9 +3,9 @@ require "helper"
 class TestJekyllFilter < Minitest::Test
   context nil do
     should "build a jekyll site where spacifier filter works" do
-      output_path = File.expand_path("./site/_site", File.dirname(__FILE__))
+      output_path = "/tmp/_site"
       `cd ./test/site && bundle install && bundle exec jekyll build --destination=#{output_path}`
-      output = File.read(File.expand_path("./site/_site/index.html", File.dirname(__FILE__)))
+      output = File.read(File.expand_path("/tmp/_site/index.html", File.dirname(__FILE__)))
       assert(true, output.include?("它在 works"))
       assert(true, output.include?("hello 世界"))
     end
