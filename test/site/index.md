b7c91594d2ac55dec6b72e936f0199e965b1e78e
David Zhang <crispgm@gmail.com>
2018-04-25 20:48:36 +0800

    Test jekyll filter

---
+++ test/site/index.md
@@ -0,0 +1,11 @@
+---
+# Feel free to add content and custom Front Matter to this file.
+# To modify the layout, see https://jekyllrb.com/docs/themes/#overriding-theme-defaults
+
+layout: home
+---
+
+{{ "它works" | spacify }}
+
+{{ "hello世界" | spacify }}
+
---
+++ test/test_jekyll_filter.rb
@@ -0,0 +1,12 @@
+require "helper"
+
+class TestJekyllFilter < Minitest::Test
+  context nil do
+    should "build a jekyll site where spacifier filter works" do
+      `cd test/site && bundle exec jekyll build`
+      output = File.read("test/site/_site/index.html")
+      assert(true, output.include?("它在 works"))
+      assert(true, output.include?("hello 世界"))
+    end
+  end
+end
