93ad197db98bd950c4392d08ff4f7ba3afa5b884
David Zhang <crispgm@gmail.com>
2018-04-25 21:23:27 +0800

    Expand path

--- test/test_jekyll_filter.rb
+++ test/test_jekyll_filter.rb
@@ -4,7 +4,7 @@ class TestJekyllFilter < Minitest::Test
   context nil do
     should "build a jekyll site where spacifier filter works" do
       `cd ./test/site && bundle install && bundle exec jekyll build`
-      output = File.read("./test/site/_site/index.html")
+      output = File.read(File.expand_path("./site/_site/index.html", File.dirname(__FILE__)))
       assert(true, output.include?("它在 works"))
       assert(true, output.include?("hello 世界"))
     end
