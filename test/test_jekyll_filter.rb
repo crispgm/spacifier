4165bb53c5f40c805ebcee551dbd3e5994450c8f
David Zhang <crispgm@gmail.com>
2018-04-25 21:14:57 +0800

    Fix relative paths

--- test/test_jekyll_filter.rb
+++ test/test_jekyll_filter.rb
@@ -3,8 +3,8 @@ require "helper"
 class TestJekyllFilter < Minitest::Test
   context nil do
     should "build a jekyll site where spacifier filter works" do
-      `cd test/site && bundle install && bundle exec jekyll build`
-      output = File.read("test/site/_site/index.html")
+      `cd ./test/site && bundle install && bundle exec jekyll build`
+      output = File.read("./test/site/_site/index.html")
       assert(true, output.include?("它在 works"))
       assert(true, output.include?("hello 世界"))
     end
