5a91f3b7154a8068a99f2ac33d3a2b42d3eb1df5
David Zhang <crispgm@gmail.com>
2018-04-26 11:53:20 +0800

    Fix jekyll filter test

--- test/test_jekyll_filter.rb
+++ test/test_jekyll_filter.rb
@@ -5,8 +5,7 @@ class TestJekyllFilter < Minitest::Test
     should "build a jekyll site where spacifier filter works" do
       output_path = "/tmp/_site"
       output = File.read(File.expand_path("/tmp/_site/index.html", File.dirname(__FILE__)))
-      puts output
-      assert_equal(true, output.include?("它在 works"))
+      assert_equal(true, output.include?("它 works"))
       assert_equal(true, output.include?("hello 世界"))
     end
   end
