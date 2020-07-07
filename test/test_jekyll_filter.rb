9df1297370593e9ffdbb83aa36adf188e82392bd
David Zhang <crispgm@gmail.com>
2020-07-07 14:38:42 +0800

    fix: test on jekyll filter

--- test/test_jekyll_filter.rb
+++ test/test_jekyll_filter.rb
@@ -3,8 +3,12 @@ require "helper"
 class TestJekyllFilter < Minitest::Test
   context nil do
     should "build a jekyll site where spacifier filter works" do
-      output_path = "/tmp/_site"
-      output = File.read(File.expand_path("/tmp/_site/index.html", File.dirname(__FILE__)))
+      output_path = "/tmp/spacifier_test"
+      Dir.chdir("./test/site") do
+        system "bundle"
+        system "bundle exec jekyll b -d #{output_path}"
+      end
+      output = File.read(File.expand_path("#{output_path}/index.html", File.dirname(__FILE__)))
       assert_equal(true, output.include?("它 works"))
       assert_equal(true, output.include?("hello 世界"))
     end
